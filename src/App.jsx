3f248ff272138e5b225276adf52f24a1e87fd6d5
Abdullah Amir <aa727dar@gmail.com>
2025-11-04 04:01:52 +0500

    Modified Background to Dynamic Background Change

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import Queue from './dataStructures/Queue'
 import Card from '@heruka_urgyen/react-playing-cards'
 import SolitaireBoard from './components/SolitaireBoard'
 import Header from './components/Header.jsx'
-import Balatro from './components/Balatro/Balatro.jsx';;
-
+import Balatro from './components/Balatro/Balatro.jsx';
+import { balatroThemes } from './components/Balatro/BalatroPreset.jsx'
 
 function App() {
   const [game] = useState(() => {
@@ -16,6 +16,15 @@ function App() {
     g.initializeGame(LinkedList, Stack, Queue);
     return g;
   });
+
+  const themeKeys = Object.keys(balatroThemes);
+  const [themeIndex, setThemeIndex] = useState(0);
+
+  const currentTheme = balatroThemes[themeKeys[themeIndex]];
+
+  const handleThemeChange = () => {
+    setThemeIndex((prev) => (prev + 1) % themeKeys.length);
+  };
 
   useEffect(() => {
     console.log("=== Initial Game State ===");
@@ -30,12 +39,15 @@ function App() {
     <>
       <div className="relative w-screen h-screen overflow-hidden bg-black">
         <Balatro
+          color1={currentTheme.color1}
+          color2={currentTheme.color2}
+          color3={currentTheme.color3}
           isRotate={false}
           mouseInteraction={false}
           pixelFilter={2000}
         />
         <div className="absolute inset-0  text-white p-6">
-          <Header />
+          <Header onThemeChange={handleThemeChange} />
           {/* <h1 className="text-3xl font-bold  text-center drop-shadow-lg">
             Solitaire Klondike
           </h1> */}
---
+++ src/components/Balatro/BalatroPreset.jsx
@@ -0,0 +1,18 @@
+// balatroThemes.js
+export const balatroThemes = {
+  classic: {
+    color1: '#2B2626',
+    color2: '#FCD8AA',
+    color3: '#2B2626'
+  },
+  sunset: {
+    color1: '#DE443B',
+    color2: '#006BB4',
+    color3: '#162325'
+  },
+  ocean: {
+    color1: '#E3D3D3',
+    color2: '#FFFFFF',
+    color3: '#9CABB4'
+  }
+};
