f0d9f5b39637c7e65ef4ccbaec5f58b1e3962e41
Abdullah Amir <aa727dar@gmail.com>
2026-02-15 11:45:28 +0500

    Added Mobile Support

--- src/components/SolitaireBoard.jsx
+++ src/components/SolitaireBoard.jsx
@@ -36,8 +36,8 @@ const mouseSensor = useSensor(MouseSensor);
     // Press and hold for 250ms or move 5px to start dragging
     // This prevents accidental drags when trying to scroll the page
     activationConstraint: {
-      delay: 250,
-      tolerance: 5,
+      delay: 150,
+      tolerance: 15,
     },
   });
 
